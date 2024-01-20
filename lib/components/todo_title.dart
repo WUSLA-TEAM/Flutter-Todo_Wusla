27f9c94cb682ef62492cf5f417daae52778e784a
wusla17 <wuslateam@gmail.com>
2024-01-20 13:24:44 +0530

    added the small chages like the delete fuction and anoithe small codes

--- lib/components/todo_title.dart
+++ lib/components/todo_title.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
-// ignore: must_be_immutable
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
-  Function(bool?)? onChanged;
-  Function(BuildContext)? deleteFunction;
+  final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     Key? key,
@@ -20,43 +19,54 @@ class ToDoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(25.0, 25, 25, 0),
-      child: Container(
-        color: Colors.yellow[500], // Set background color here
-        child: Slidable(
-          endActionPane: ActionPane(motion: StretchMotion(), children: [
-            SlidableAction(
-              onPressed: deleteFunction,
-              icon: Icons.delete,
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-              backgroundColor: Colors.red.shade800,
-            )
-          ]),
-          child: Container(
-            padding: const EdgeInsets.all(24),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Row(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.yellow[500],
+          ),
+          child: Slidable(
+            endActionPane: ActionPane(
+              motion: StretchMotion(),
+              extentRatio: 0.2,
               children: [
-                // Checkbox
-                Checkbox(
-                  value: taskCompleted,
-                  onChanged: onChanged,
-                  activeColor: Colors.yellow[800],
-                ),
-                // Text
-                Text(
-                  taskName,
-                  style: TextStyle(
-                    decoration: taskCompleted
-                        ? TextDecoration.lineThrough
-                        : TextDecoration.none,
+                SlidableAction(
+                  onPressed: deleteFunction,
+                  icon: Icons.delete,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(12),
+                    bottomRight: Radius.circular(12),
                   ),
-                ),
+                  backgroundColor: Colors.red.shade800,
+                )
               ],
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 12,
+              ),
+              child: Row(
+                children: [
+                  Checkbox(
+                    value: taskCompleted,
+                    onChanged: onChanged,
+                    activeColor: Colors.yellow[800],
+                  ),
+                  SizedBox(width: 12),
+                  Expanded(
+                    child: Text(
+                      taskName,
+                      style: TextStyle(
+                        decoration: taskCompleted
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
