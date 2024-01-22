528abbd83dee195f4c1b26d8ef55c7f9a6377d18
wusla17 <wuslateam@gmail.com>
2024-01-22 08:15:23 +0530

    added new fetures like bottom sheet and plane to add the time picker will also help trhe user to make it easy

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:wusla/components/bottom_sheet.dart';
 import 'package:wusla/components/dilog_box.dart';
 import 'package:wusla/components/todo_title.dart';
 import 'package:wusla/data/databse.dart';
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
@@ -81,44 +84,75 @@ class _HomeState extends State<Home> {
     db.updateDatabse();
   }
 
+  // Future _displayBottomSheet(BuildContext context) {
+  //   return showModalBottomSheet(
+  //     context: context,
+  //     builder: (context) => Container(
+  //       height: 200,
+  //     ),
+  //   );
+  // }
+
+  void _showBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return BottomSheetContent(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () {
+            // Handle Cancel button pressed
+            Navigator.of(context).pop(); // Close the bottom sheet
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
-      home: Scaffold(
-        backgroundColor: Color(0xFF2BB7E3),
-        appBar: AppBar(
+      home: GestureDetector(
+        onTap: () {
+          // Close the keyboard when tapping outside of the text field or bottom sheet
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Scaffold(
           backgroundColor: Color(0xFF2BB7E3),
-          title: const Center(
-            child: Text(
-              'To Do',
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white),
+          appBar: AppBar(
+            backgroundColor: Color(0xFF2BB7E3),
+            title: const Center(
+              child: Text(
+                'To Do',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
             ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: createNewTask,
-          backgroundColor: Color(0xFF351A87),
-          child: const Icon(
-            Icons.add,
-            color: Color(0xFFFFFFFF),
+          floatingActionButton: FloatingActionButton(
+            onPressed: _showBottomSheet,
+            backgroundColor: Color(0xFF351A87),
+            child: const Icon(
+              Icons.add,
+              color: Color(0xFFFFFFFF),
+            ),
           ),
-        ),
-        body: ListView.builder(
-          itemCount: db.todoList.length,
-          itemBuilder: (context, index) {
-            return ToDoTile(
-              taskCompleted: db.todoList[index][1],
-              taskName: db.todoList[index][0],
-              onChanged: (value) => checkboxChanged(value, index),
-              deleteFunction: (context) {
-                deleteTask(index);
-              },
-            );
-          },
+          body: ListView.builder(
+            itemCount: db.todoList.length,
+            itemBuilder: (context, index) {
+              return ToDoTile(
+                taskCompleted: db.todoList[index][1],
+                taskName: db.todoList[index][0],
+                onChanged: (value) => checkboxChanged(value, index),
+                deleteFunction: (context) {
+                  deleteTask(index);
+                },
+              );
+            },
+          ),
         ),
       ),
     );
---
+++ lib/components/bottom_sheet.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:wusla/components/mybutton.dart';
+
+class BottomSheetContent extends StatefulWidget {
+  final TextEditingController controller;
+  final VoidCallback onSave;
+  final VoidCallback onCancel;
+
+  const BottomSheetContent({
+    Key? key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  }) : super(key: key);
+
+  @override
+  _BottomSheetContentState createState() => _BottomSheetContentState();
+}
+
+class _BottomSheetContentState extends State<BottomSheetContent> {
+  double _bottomSheetHeight = 1000;
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.all(40),
+        height: _bottomSheetHeight,
+        decoration: const BoxDecoration(
+          color: Color(0xFF351A87),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // TextField with the provided TextEditingController
+            TextField(
+              style: const TextStyle(
+                color: Color(0xFFFFFFFF),
+                fontWeight: FontWeight.bold,
+              ),
+              decoration: InputDecoration(
+                enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                focusedBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFF2BB7E3),
+                    width: 2,
+                  ), // Wider for focus
+                ),
+                contentPadding: const EdgeInsets.all(12),
+                border: UnderlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: const BorderSide(
+                    style: BorderStyle.solid,
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                hintText: 'Enter your text',
+                hintStyle: const TextStyle(color: Colors.white),
+              ),
+              controller: widget.controller, // Use the provided controller
+            ),
+
+            const SizedBox(height: 30),
+
+            // Buttons (Save and Cancel)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Save button
+                MyButton(
+                  text: 'Save',
+                  onPressed: () {
+                    setState(() {
+                      _bottomSheetHeight = 2000; // Reset height
+                    });
+                    widget.onSave();
+                  },
+                ),
+
+                const SizedBox(width: 10),
+
+                // Cancel button
+                MyButton(
+                  text: 'Cancel',
+                  onPressed: () {
+                    setState(() {
+                      _bottomSheetHeight = 200; // Reset height
+                    });
+                    widget.onCancel();
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
